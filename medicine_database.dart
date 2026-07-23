39e5cfadcf58da465929ca37c4cf442c4f0becd0
ahad424 <mdahadsorder84@gmail.com>
2026-07-24 02:23:33 +0600

    Create README.md

---
+++ medicine_database.dart
@@ -0,0 +1,19 @@
+import 'medicine_model.dart';
+
+List<Medicine> medicines = [
+  Medicine(
+    name: "Napa",
+    generic: "Paracetamol",
+    use: "জ্বর ও ব্যথা",
+  ),
+  Medicine(
+    name: "Seclo",
+    generic: "Omeprazole",
+    use: "গ্যাস্ট্রিক",
+  ),
+  Medicine(
+    name: "Ace",
+    generic: "Paracetamol",
+    use: "জ্বর",
+  ),
+];
