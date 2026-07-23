3601da22e9d31f7ef91fa1d61886fbc0023f2660
ahad424 <mdahadsorder84@gmail.com>
2026-07-24 02:24:00 +0600

    Create README.md

---
+++ doctor_model.dart
@@ -0,0 +1,11 @@
+class Doctor {
+  final String name;
+  final String specialty;
+  final String phone;
+
+  Doctor({
+    required this.name,
+    required this.specialty,
+    required this.phone,
+  });
+}
