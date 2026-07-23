c5ed14c877922a0f5e5643a058743943777914a7
ahad424 <mdahadsorder84@gmail.com>
2026-07-24 02:22:21 +0600

    Create README.md

---
+++ medicine_model.dart
@@ -0,0 +1,11 @@
+class Medicine {
+  final String name;
+  final String generic;
+  final String use;
+
+  Medicine({
+    required this.name,
+    required this.generic,
+    required this.use,
+  });
+}
