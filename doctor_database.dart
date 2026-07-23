b412dbf7265cc2ffb328d8fd165d481a077e0b71
ahad424 <mdahadsorder84@gmail.com>
2026-07-24 02:25:43 +0600

    Create README.md

---
+++ doctor_database.dart
@@ -0,0 +1,14 @@
+import 'doctor_model.dart';
+
+List<Doctor> doctors = [
+  Doctor(
+    name: "ডা. রহমান",
+    specialty: "মেডিসিন",
+    phone: "01700000000",
+  ),
+  Doctor(
+    name: "ডা. করিম",
+    specialty: "শিশু বিশেষজ্ঞ",
+    phone: "01800000000",
+  ),
+];
