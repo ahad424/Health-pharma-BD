5cb9435115b3303f4094cf1fd2004f9df4671168
ahad424 <mdahadsorder84@gmail.com>
2026-07-24 02:26:54 +0600

    Create README.md

---
+++ hospital_database.dart
@@ -0,0 +1,12 @@
+import 'hospital_model.dart';
+
+List<Hospital> hospitals = [
+  Hospital(
+    name: "Dhaka Medical College Hospital",
+    address: "ঢাকা",
+  ),
+  Hospital(
+    name: "Narayanganj General Hospital",
+    address: "নারায়ণগঞ্জ",
+  ),
+];
